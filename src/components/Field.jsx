113153559f92303d00fefa8f2b4b114ec8e6a02c
Julian P <julian-peukert@web.de>
2024-11-25 15:25:14 +0100

    add: Field component / teststyling

---
+++ src/components/Field.jsx
@@ -0,0 +1,10 @@
+export const Field = () => {
+  return (
+    <>
+      <div className="battlefield">
+        <div className="enemy-container"></div>
+        <div className="player-container"></div>
+      </div>
+    </>
+  )
+}
