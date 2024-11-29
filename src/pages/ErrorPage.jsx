7b4418049d0a24c5c87ca894b27c615fd98771d2
Julian P <julian-peukert@web.de>
2024-11-29 09:31:04 +0100

    add: Error page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+export const ErrorPage = () => {
+  return (
+    <>
+      <h1>Error Page</h1>
+    </>
+  )
+}
--- src/utils/AppRouter.jsx
+++ src/utils/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { Teambuilder } from "../pages/Teambuilder";
+import { ErrorPage } from "../pages/ErrorPage";
 
 export const AppRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: ,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/teambuilder",
